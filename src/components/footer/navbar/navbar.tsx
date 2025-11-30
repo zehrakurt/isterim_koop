4d2d95a480b3202c719058bc653d6b825fb6a854
zehrakurt <153917389+zehrakurt@users.noreply.github.com>
2025-11-30 16:48:35 +0300

    .

--- src/components/footer/navbar/navbar.tsx
+++ src/components/footer/navbar/navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
-import "./Navbar.css";
+import "./navbar.css";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
